60dd073e718cd123ca99bd94a1e9c22a96a9aecd
FrederikBjorn <fbjoern18@gmail.com>
2024-10-25 15:29:54 +0200

    initial empty react project

---
+++ README.old.md
@@ -0,0 +1,1 @@
+"# safe-space-react-2024" 
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Safe Space
+      </header>
+    </div>
+  );
+}
+
+export default App;
