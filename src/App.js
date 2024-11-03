b57334e02b1ff769a7e31d318f211b47a6004d2e
FrederikBjorn <fbjoern18@gmail.com>
2024-11-03 09:45:36 +0100

    add-pase-for-back4app-integration
    just the boilerplate imports for the guide;
    https://www.back4app.com/docs/react/quickstart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import LandingPageHero from "./Pages/LandingPage/LandingPageHero";
 import { Routes, Route } from "react-router-dom";
 import LoginPatientUser from "./Pages/LandingPage/loginPatientUser";
 import LoginProfessionalUser from "./Pages/LandingPage/loginProfessionalUser";
+import Parse from "parse/dist/parse.min.js";
+
+// Your Parse initialization configuration goes here
+const PARSE_APPLICATION_ID = "YOUR_APPLICATION_ID_HERE";
+const PARSE_HOST_URL = "https://parseapi.back4app.com/";
+const PARSE_JAVASCRIPT_KEY = "YOUR_JAVASCRIPT_KEY_HERE";
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
