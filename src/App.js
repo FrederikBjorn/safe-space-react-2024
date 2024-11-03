6415bc38e30944542b7dc64ccd1b04d53ea8ca08
FrederikBjorn <fbjoern18@gmail.com>
2024-11-03 10:14:18 +0100

    added parse, but npm won't start

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import LandingPageHero from "./Pages/LandingPage/LandingPageHero";
 import { Routes, Route } from "react-router-dom";
 import LoginPatientUser from "./Pages/LandingPage/loginPatientUser";
 import LoginProfessionalUser from "./Pages/LandingPage/loginProfessionalUser";
-import Parse from "parse/dist/parse.min.js";
-
-// Your Parse initialization configuration goes here
-const PARSE_APPLICATION_ID = "YOUR_APPLICATION_ID_HERE";
-const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-const PARSE_JAVASCRIPT_KEY = "YOUR_JAVASCRIPT_KEY_HERE";
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+//import Parse from "parse";
+//
+//// Your Parse initialization configuration goes here
+//const PARSE_APPLICATION_ID = "QuGtog6ad13b02XIRbK4johVN1xoqHc334Yj3a48"; //from back4app --> app settings --> security & keys
+//const PARSE_HOST_URL = "https://parseapi.back4app.com/"; //form server settings --> host url in the back4app platform
+//const PARSE_JAVASCRIPT_KEY = "rMsoyGV5w8xzNM59cuLAx4lX7LkwMnNdLNC1EUSB"; //from back4app --> app settings --> security & keys
+//Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+//Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
