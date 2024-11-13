64ca16a96bc97afae9c74fd757b9ad82ee811a98
jonpe <jonpe@itu.dk>
2024-11-13 13:03:26 +0100

    cleanUp2

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,30 +5,10 @@ import "./Navbar.css";
 import Parse from "parse";
 
 function Navbar() {
-  const [authenticatedUser, setAuthenticatedUser] = useState(false); // assuming false means not authenticated
-
-  // Check if the user is authenticated
-  useEffect(() => {
-    const currentUserCheck = Parse.User.current();
-
-    if (currentUserCheck === !null) {
-      console.log("currentUser is not null");
-      if (currentUserCheck.authenticated()) {
-        console.log("currentUserCheck = true");
-        setAuthenticatedUser(true);
-      } else {
-        console.log("currentUserCheck = false");
-        setAuthenticatedUser(false);
-      }
-    } else {
-      console.log("currentUser is null");
-    }
-  }, []);
-
   return (
     <>
-      <DesktopNavigation authenticatedUser={authenticatedUser} />
-      <MobileNavigation authenticatedUser={authenticatedUser} />
+      <DesktopNavigation />
+      <MobileNavigation />
     </>
   );
 }
