47fc7b1aaea1acaa819dbdad7e8e589945204d18
jonpe <jonpe@itu.dk>
2024-12-04 13:36:37 +0100

    Everything is working!

--- src/Components/Navbar/DesktopNavigation.js
+++ src/Components/Navbar/DesktopNavigation.js
@@ -3,15 +3,23 @@ import NavlinksAfterLogin from "./NavlinksAfterLogin.js";
 import NavlinksBeforeLogin from "./NavlinksBeforeLogin.js";
 import "./Navbar.css";
 import { useUserStore } from "../UserData/useUserStore.js";
+import { useNavigate } from "react-router-dom";
 
 function DesktopNavigation() {
   const { currentUser } = useUserStore();
+  const navigate = useNavigate();
 
   return (
     <div className="navbar" id="navbar-desktop">
       <nav className="DesktopNavigation">
         <div className="logo">
-          <img src="images/icon.png" alt="logo" width="100" height="90" />
+          <img
+            src="images/icon.png"
+            alt="logo"
+            width="100"
+            height="90"
+            onClick={() => navigate("/")}
+          />
         </div>
         {currentUser ? <NavlinksAfterLogin /> : <NavlinksBeforeLogin />}
       </nav>
