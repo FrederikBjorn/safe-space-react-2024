124070a13696cd0e47248824e7f0ca0762fd17a6
emilHojgaard <emilhn94@gmail.com>
2024-12-25 16:53:02 +0100

    jspdf and some functionality added

--- src/Pages/DiaryPage/DiaryPage.js
+++ src/Pages/DiaryPage/DiaryPage.js
@@ -1,5 +1,42 @@
 import "./DiaryPage.css"
+import { useState } from "react";
+import jsPDF from "jspdf";
+import { v4 as uuidv4 } from 'uuid';
+
+
 function DiaryPage() {
+    const [text, setText] = useState("");
+    const [history, setHistory] = useState([]);
+
+    const handleAddEntry = function(){
+        if(text === "") return;
+
+        // Converts text to a PDF: 
+        const pdf = new jsPDF();
+        const filename = new Date().toISOString().slice(0, 10) + ".pdf";
+
+        pdf.text(text, 10, 10);
+        pdf.save(filename);
+        setHistory([...history, {name: filename, file: pdf}]);
+        setText("");
+        console.log(history);
+    }
+
+    const handleDismiss = function(){
+        setText("");
+    }
+
+    const handleDownload = (url) => {
+        try {
+          window.open(url, "_blank");
+        } catch (error) {
+          console.error("Error opening file in new tab:", error);
+        }
+      };
+
+    const formatName = (name) => {
+        return name.split("_").slice(1).join("_");
+      };
 
     return(
         <div className="diary">
@@ -12,11 +49,11 @@ function DiaryPage() {
                     </div>
 
                     <div className="diary-body">
-                        <textarea placeholder="How are you feeling today?"/>
+                        <textarea placeholder="How are you feeling today?" value = {text} onChange={(e)=>setText(e.target.value)}/>
                     </div>
                     <div className="entry-buttons">
-                        <button>Add Diary Entry</button>
-                        <button> Dismiss</button>
+                        <button onClick={handleAddEntry}>Add Diary Entry</button>
+                        <button onClick={handleDismiss}>Dismiss</button>
                     </div>
                 </div>
                 <div className="diary-history-container">
@@ -26,9 +63,22 @@ function DiaryPage() {
                     </div>
             
                     <ul className="items">
-                        <li>item 1</li>
-                        <li>item 2</li>
-                        <li>item 3</li>
+                        {history.map((file)=>(
+                            <li key={uuidv4()} className="entry">
+                              <img src="Images/file.png" alt=""/>
+                              <span
+                                className="message-text"
+                                onClick={() => handleDownload(file.url())}
+                              >
+                                {file.name}
+                              </span>
+                            <img
+                              src="Images/downloade.png"
+                              alt=""
+                              onClick={() => handleDownload(file.url())}
+                            />
+                          </li>
+                        ))}
                     </ul>
                     <div className="download-history">
                         <button>Download All</button>
