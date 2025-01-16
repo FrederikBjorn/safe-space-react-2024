77fb3f81f4eaad1d3f4e024acdc40615ff4240b7
jonpe <jonpe@itu.dk>
2025-01-16 11:50:41 +0100

    Ensuring visibility of system status in the navigation bar

--- src/Components/Navbar/NavlinksAfterLogin.js
+++ src/Components/Navbar/NavlinksAfterLogin.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useUserLogOut } from "../Authentication/useUserLogOut";
 
 function NavlinksAfterLogin({ isClicked, closeMenu }) {
   const { userLogOut } = useUserLogOut();
+  const [isActive, setActive] = useState("homepage");
+  const location = useLocation();
+
   const handleLogout = async () => {
     await userLogOut();
     if (isClicked) {
@@ -12,14 +15,29 @@ function NavlinksAfterLogin({ isClicked, closeMenu }) {
     }
   };
 
+  useEffect(() => {
+    const path = location.pathname.substring(1);
+    setActive(path || "homepage");
+  }, [location]);
+
   return (
     <nav className="navlinks">
       <ul>
         <li onClick={() => isClicked && closeMenu()}>
-          <Link to="/homepage">Home</Link>
+          <Link
+            className={` ${isActive === "homepage" ? "active-link" : ""}`}
+            to="/homepage"
+          >
+            Home
+          </Link>
         </li>
         <li onClick={() => isClicked && closeMenu()}>
-          <Link to="/chatpage">Chat</Link>
+          <Link
+            className={` ${isActive === "chatpage" ? "active-link" : ""}`}
+            to="/chatpage"
+          >
+            Chat
+          </Link>
         </li>
         <li onClick={() => isClicked && closeMenu()}>
           <Link to="/diarypage">Diary</Link>
--- src/Components/Navbar/NavlinksBeforeLogin.js
+++ src/Components/Navbar/NavlinksBeforeLogin.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function NavlinksBeforeLogin({ isClicked, closeMenu }) {
+  const [isActive, setActive] = useState("homepage");
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname.substring(1); // Remove leading slash
+    setActive(path || "homepage");
+  }, [location]);
+
   return (
     <nav className="navlinks">
       <ul>
         <li onClick={() => isClicked && closeMenu()}>
-          <Link to="/loginpage">Log in</Link>
+          <Link
+            className={` ${isActive === "loginpage" ? "active-link" : ""}`}
+            to="/loginpage"
+          >
+            Log in
+          </Link>
         </li>
       </ul>
     </nav>
